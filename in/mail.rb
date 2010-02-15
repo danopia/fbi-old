409354349ec8b076e8cf4e29b87b6846dbb4bf41
Daniel Danopia <me@danopia.net>
2010-02-14 23:19:55 -0500

    Clean up some SMTP server code

--- in/mail.rb
+++ in/mail.rb
@@ -1,12 +1,12 @@
 require File.join(File.dirname(__FILE__), '..', 'common', 'client')
 
 class MailServer < FBI::LineConnection
-  HOSTNAME = 'fbi.danopia.net'
+  attr_accessor :hostname
   
-  def post_init
+  def post_init hostname=nil
     super
-    
-    send_line "220 #{HOSTNAME} ESMTP FBIMail 0.0.1; Mail Receiver Ready"
+    @hostname = hostname || `hostname`.chomp
+    send_line "220 #{@hostname} ESMTP FBIMail 0.0.1; Mail Receiver Ready"
   end
   
   def send_line data
@@ -22,11 +22,11 @@ class MailServer < FBI::LineConnection
       case args.first.upcase
       
         when 'HELO'
-          send_line "250 #{HOSTNAME} at your service"
+          send_line "250 #{hostname} at your service"
           
         when 'EHLO'
-          send_line "250-#{HOSTNAME} at your service, [127.0.0.1]"
-          send_line "250-SIZE 35651584"
+          send_line "250-#{hostname} at your service"
+          send_line "250-SIZE 35651584" # heh? got this from google
           send_line "250-8BITMIME"
           send_line "250-ENHANCEDSTATUSCODES"
           send_line "250 PIPELINING"
@@ -39,9 +39,9 @@ class MailServer < FBI::LineConnection
         when 'RCPT'
           args[1] =~ /^TO:\<(.+)\>$/i
           @to = $1 || args[2][1..-2]
-          if @to.include? HOSTNAME
+          if @to.include? hostname
             send_line "250 2.1.5 OK"
-          else
+          else # don't want to be marked as an open relay
             send_line "550 5.1.1 The email account that you tried to reach does not exist."
           end
         
@@ -51,11 +51,9 @@ class MailServer < FBI::LineConnection
           send_line "354  Go ahead"
           
         when 'QUIT'
-          send_line "221 2.0.0 #{HOSTNAME} closing connection"
+          send_line "221 2.0.0 #{hostname} closing connection"
           close_connection
-          
       end
-    
     elsif line == '.'
       @in_message = false
       got_mail
@@ -67,39 +65,26 @@ class MailServer < FBI::LineConnection
   end
   
   def got_mail
-    puts @message.size
-    File.open('mail.txt', 'w') {|f| f.puts @message }
+    #File.open('mail.txt', 'w') {|f| f.puts @message }
     if @message.include?('Log Message:') && @from.include?('sourceforge.net')
       
-      p @message =~ /^Revision: ([0-9]+)$/
+      @message =~ /^Revision: ([0-9]+)$/
       rev = $1.to_i
       
-      p @message =~ /(http:\/\/.+\.sourceforge\.net\/(.+)\/\?rev=[0-9]+&view=rev)/
-      url = $1
-      project = $2
+      @message =~ /(http:\/\/.+\.sourceforge\.net\/(.+)\/\?rev=[0-9]+&view=rev)/
+      url, project = $1, $2
       
-      p @message =~ /^Author: +(.+)$/
+      @message =~ /^Author: +(.+)$/
       author = $1
       
-      p @message =~ /^Subject: SF.net .+: .+:\[[0-9]+\] +(.+)$/
+      @message =~ /^Subject: SF.net .+: .+:\[[0-9]+\] +(.+)$/
       path = $1
       
       index = @message.index("Log Message:") + 20
       index = @message.index("\n", index) + 1
       end_index = @message.index("\n\nModified Paths:") - 1
-      p message = @message[index..end_index]
+      message = @message[index..end_index]
       
-      p({
-        :project => project,
-        :owner => nil,
-        :fork => false,
-        :author => {:email => nil, :name => author},
-        :branch => path,
-        :commit => "r#{rev}",
-        :message => message,
-        :url => url,
-      })
-
       FBI::Client.publish 'commits', [{
         :project => project,
         :owner => nil,
