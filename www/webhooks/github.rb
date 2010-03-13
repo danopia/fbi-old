9ceff8730ba7d1dcf5ae05db1361870e747daebb
Daniel Danopia <me@danopia.net>
2010-03-12 19:06:19 -0500

    Remove old, commented-out code

--- www/webhooks/github.rb
+++ www/webhooks/github.rb
@@ -6,31 +6,7 @@ class GithubHook
     data = env['rack.input'].read
     data = JSON.parse CGI::unescape(data[8..-1])
     
-    # merge floods
-    #~ if data['commits'].size > 3
-      #~ dropped = data['commits'].size - 3
-      #~ data['commits'].shift until data['commits'].size == 3
-      #~ data['commits'].first['message'] = "(#{dropped} previous commit[s] dropped --FBI) " + data['commits'].first['message']
-    #~ end
-    #~ 
-    #~ data['commits'].pop if data['commits'].size > 1 && data['commits'].last['message'] =~ /^Merge remote branch/ && data['repository']['fork']
-    
     output = data['commits'].map do |commit|
-    
-      # previous commit?
-      #~ dup = `grep #{commit['id']} sha1s.txt`.size > 0
-      #~ if not dup
-        #~ `echo #{commit['id']} >> sha1s.txt`
-        #~ 
-        #~ dup = `grep #{commit['timestamp']} timestamps.txt`.size > 0
-        #~ `echo #{commit['timestamp']} >> timestamps.txt` unless dup
-      #~ end
-      #~ 
-      #~ if dup
-        #~ next if data['repository']['fork'] || !(commit['message'] =~ /^Merge remote branch/)
-        #~ commit['message'] << ' (merged into upstream from fork --FBI)'
-      #~ end
-      
       {
         :project => data['repository']['name'],
         :owner => data['repository']['owner']['name'],
