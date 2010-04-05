bba92146198addfaf1ff53a57c0bd0a2536070b8
Daniel Danopia <me@danopia.net>
2010-04-04 20:21:30 -0400

    Made the CLU a bit easier (and less enter-intensitive)

--- cli.rb
+++ cli.rb
@@ -13,28 +13,42 @@ client = FBI::Client.new 'cli', 'hil0l'
 Thread.new { begin; client.start_loop; rescue => ex; puts ex,ex.message,ex.backtrace; end }
 
 while true
-  action = gets.chomp
-  next if action.size == 0
+  params = gets.chomp.split
+  next if params.empty?
+  action = params.shift.downcase
   
   case action
     when 'subscribe', 'join', 'sub'
-      client.send_object 'subscribe', :channels => gets.chomp.split
+      client.send_object 'subscribe',
+        :channels => params
       
     when 'unsubscribe', 'leave', 'part', 'unsub'
-      client.send_object 'unsubscribe', :channels => gets.chomp.split
+      client.send_object 'unsubscribe',
+        :channels => params
       
     when 'publish', 'send'
-      client.send_object 'publish', :target => gets.chomp, :data => JSON.parse(gets)
+      client.send_object 'publish',
+        :target => params.shift,
+        :data => JSON.parse(params.any? ? params.join(' ') : gets)
     
     when 'disconnect', 'channels', 'subscriptions', 'components'
       client.send_object action, {}
     
     when 'dblist'
-      client.send '#db', :method => 'select', :table => 'users'
+      client.send '#db',
+        :method => 'select',
+        :table => params.shift
       
-    when 'dbins'
-      client.send '#db', :method => 'insert', :table => 'users', :record => 
-        {:username => 'danopia', :email => 'test@danopia.net', :password_hash => '', :salt => '', :created_at => Time.now}
+    #~ when 'dbins'
+      #~ client.send '#db',
+        #~ :method => 'insert',
+        #~ :table => 'users',
+        #~ :record => 
+          #~ {:username => 'danopia',
+           #~ :email => 'test@danopia.net',
+           #~ :password_hash => '',
+           #~ :salt => '',
+           #~ :created_at => Time.now}
     
     else
       print "JSON for #{action} (or nothing): "
