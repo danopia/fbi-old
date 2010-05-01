549348684beb90ba9dc50e64ed214564ab5e7651
Daniel <danopia@lappy.home.danopia.net>
2010-05-01 15:12:25 -0400

    Added a migration to improve the commits table (INVOLVES DROPPING IT) so that I can start storing commits

---
+++ db/migrations/005_better_commits.rb
@@ -0,0 +1,20 @@
+class BetterCommitsMigration
+  def self.run db
+    db.sequel.drop_table :commits
+    db.sequel.create_table :commits do
+      primary_key :id
+      
+      foreign_key :repo_id, :repos
+      foreign_key :author_id, :user
+      foreign_key :identity_id, :identities
+      
+      String :message, :null => false
+      String :hash, :null => false
+      String :json, :null => false
+      
+      Time :created_at, :null => false
+      Time :commited_at, :null => false
+      Time :modified_at
+    end
+  end
+end
